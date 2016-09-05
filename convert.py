eb14f1a24a4339791b09f08bd2a846f733749fd7
globedasher <globe.dasher@gmail.com>
2016-09-04 19:00:54 -0700

    More updated to convert.py

--- convert.py
+++ convert.py
@@ -3,12 +3,74 @@ This is a new applicaiton I am wiring to manipulate an image.
 By Richard Morley
 """
 
-from PIL import Image
+import tkinter
+from tkinter import *
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+
+class App:
+    """
+    Create a class for the application that will display a window with widgets
+    to display and manipulate the image as desired.
+    """
+
+    def __init__(self, master, picture):
+        # Create the initial window frame and populate it with buttons to
+        # control the application.
+        frame = Frame(master)
+        frame.pack()
+
+        self.hi_there = Button (frame, text="Bart!", command=self.bart)
+        self.hi_there.pack(side=LEFT)
+
+        self.picb = Button (frame, text="Show Image", command=self.displayPic)
+        self.picb.pack(side=LEFT)
+
+        # Convert to Red button
+        self.invB = Button (frame, text="Convert to Red", 
+                command=img.convert_image_to_red)
+        self.invB.pack(side=LEFT)
+        
+        # Convert to Green button
+        self.invB = Button (frame, text="Convert to Green", 
+                command=img.convert_image_to_green)
+        self.invB.pack(side=LEFT)
+        
+        # Convert to Blue button
+        self.invB = Button (frame, text="Convert to Blue", 
+                command=img.convert_image_to_blue)
+        self.invB.pack(side=LEFT)
+
+        self.button = Button (
+                frame, text="Quit", fg="red", command=frame.quit
+                )
+        self.button.pack(side=LEFT)
+
+    def bart(self):
+        print("Doh!")
+
+    def data_input(self):
+        #number = self.entry
+        #print(number)
+        return
+
+    def displayPic(self):
+        # Create a canvas and display an image in the window.
+        pos = img.pic.width // 2, img.pic.height // 2
+        w = Canvas(root, width=img.pic.width, height=img.pic.height)
+        w.create_image(pos, image=photo)
+        w.pack()
+
 
 class Convert:
+    """
+    The Convert class is used to filter an image so only red, green or blue is
+    left in the saved image.
+    """
 
-    def __init__(self, img):
-        self.pic = img
+    def __init__(self, original_picture):
+        self.pic = original_picture
 
     def convert_image_to_red(self):
         # Open the image to be altered.
@@ -58,5 +120,13 @@ class Convert:
         # Save the altered image.
         self.pic.save('saved.jpg')
 
+
+# Open the original picture.
 img = Convert(Image.open('original.jpg'))
-img.convert_image_to_red()
+# Create the tkinter window.
+root = Tk()
+# Create an ImageTk.PhotoImage of the original photo.
+photo = ImageTk.PhotoImage(img.pic)
+app = App(root, photo)
+root.mainloop()
+root.destroy() #destroy the window
