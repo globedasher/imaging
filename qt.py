d9a76139f659584ee1c0e1edbb405c1f5daba100
globedasher <globe.dasher@gmail.com>
2016-09-05 21:26:44 -0700

    More updates to the Qt application.

--- qt.py
+++ qt.py
@@ -1,12 +1,17 @@
+#!/urs/bin/python3
+#-*- coding: utf-8 -*-
+
 """
 PyQt5 Hello, World.
-Richard Morley
+
+author: Richard Morley
 """
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QIcon
 
 # Import Image handling libraries.
@@ -16,13 +21,36 @@ from PIL import Image
 import convert
 
 
-def main():
-    img = convert.Convert(Image.open('original.jpg'))
-    app = QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    button = QtWidgets.QPushButton('Hello, World!')
-    window.setCentralWidget(button)
-    window.show()
-    app.exec_()
+class Editor(QWidget):
 
-main()
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        
+        #app = QApplication(sys.argv)
+        #window = QtWidgets.QMainWindow()
+        #img = convert.Convert(Image.open('original.jpg'))
+        
+        red_btn = QPushButton('Make Red', self)
+        red_btn.clicked.connect(convert.Convert.convert_image_to_red)
+        red_btn.resize(red_btn.sizeHint())
+        red_btn.move(50, 20)
+
+        button = QPushButton('Quit', self)
+        button.clicked.connect(QCoreApplication.instance().quit)
+        button.resize(button.sizeHint())
+        button.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Image Editor')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ed = Editor()
+    sys.exit(app.exec_())
